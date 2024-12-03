e7041c00760b8b94ab830c1df5a1668ecce8503e
aksiazek <adam.ksiazek.pmpg@gmail.com>
2024-12-03 10:15:35 +0100

    refactor

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,8 +5,8 @@ const router = express.Router();
 router.post('/login', (req, res) => {
     const { username, password } = req.body;  // Oczekujemy w ciele żądania nazwy użytkownika i hasła
 
-    const envUsername = process.env.JWT_USER_LOGIN_TEST;
-    const envUserPss = process.env.JWT_USER_PASS_TEST;
+    const envUsername = process.env.JWT_USER_LOGIN;
+    const envUserPss = process.env.JWT_USER_PASS;
 
     if (username === envUsername && password === envUserPss) {
         const user = {
