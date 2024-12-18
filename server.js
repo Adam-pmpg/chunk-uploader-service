2cabd0c09875d8fd9aa88b38a53992d3a1f9e093
aksiazek <adam.ksiazek.pmpg@gmail.com>
2024-12-18 11:24:54 +0100

    refactor

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const express = require('express');
-const cors = require('cors');
 const corsMiddleware = require('./middleware/corsMiddleware');
 const authenticateJWT = require('./middleware/authenticateJWT');
 const bodyParser = require('body-parser');
