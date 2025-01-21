3952ec31042270bb075d41e6bde0b2242d3ca62a
Adam-pmpg <adam.ksiazek.pmpg@gmail.com>
2025-01-21 12:16:33 +0100

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,10 +3,27 @@ const { generateToken } = require('../auth/tokenGenerator');
 const router = express.Router();
 
 router.post('/login', (req, res) => {
-    const { username, password } = req.body;  // Oczekujemy w ciele żądania nazwy użytkownika i hasła
+    let username = '';
+    let password = '';
+    try {
+        const authHeader = req.headers['authorization'];
+        if (authHeader && authHeader.startsWith('Basic ')) {
+            const base64Credentials = authHeader.split(' ')[1];
+            const credentials = Buffer.from(base64Credentials, 'base64').toString('utf8');
+            [username, password] = credentials.split(':');
+        } else {
+            // Pobieranie danych z ciała żądania
+            username = req.body?.username || 'brak-usera';
+            password = req.body?.password || 'brak-password';
+        }
+    } catch (error) {
+        console.log({
+            message: `Error przy pobieraniu bearer: ${error.message}`,
+        });
+    }
 
-    const envUsername = process.env.JWT_USER_LOGIN;
-    const envUserPss = process.env.JWT_USER_PASS;
+    const envUsername = process.env.CHUNK_SERVICE_JWT_USER_LOGIN;
+    const envUserPss = process.env.CHUNK_SERVICE_JWT_USER_PASS;
 
     if (username === envUsername && password === envUserPss) {
         const user = {
